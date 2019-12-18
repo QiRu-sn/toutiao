79a89d322abc7e29094ac216f71f250c1fa64c4f
QiRu <916331669@qq.com>
2019-12-18 21:50:50 +0800

    登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,19 +4,19 @@
           <div class="login-logo">
               <img src="../../assets/img/logo_index.png" alt="">
           </div>
-          <el-form>
-              <el-form-item>
-                  <el-input placeholder="请输入手机号"></el-input>
+          <el-form :model='ruleForm' :rules='rules' ref="myForm">
+              <el-form-item prop="phone">
+                  <el-input v-model="ruleForm.phone" placeholder="请输入手机号"></el-input>
               </el-form-item>
-              <el-form-item>
-                  <el-input placeholder="验证码" style="width:68%"></el-input>
+              <el-form-item prop="code">
+                  <el-input v-model="ruleForm.code" placeholder="验证码" style="width:68%"></el-input>
                   <el-button plain style="float:right;width:30%">发送验证码</el-button>
               </el-form-item>
-              <el-form-item>
-                  <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
+              <el-form-item prop="check">
+                  <el-checkbox v-model="ruleForm.check" >我已阅读并同意用户协议和隐私条款</el-checkbox>
               </el-form-item>
               <el-form-item>
-                  <el-button type="primary" style="width:100%">登录</el-button>
+                  <el-button @click='login' type="primary" style="width:100%">登录</el-button>
               </el-form-item>
           </el-form>
       </el-card>
@@ -27,7 +27,28 @@
 export default {
   data () {
     return {
-      checked: true
+      ruleForm: {
+        phone: '',
+        code: '',
+        check: false
+      },
+      rules: {
+        phone: [{ required: true, message: '请输入手机号码', trigger: 'blur' }, { pattern: /^1[3456789]\d{9}$/ }],
+        code: [{ required: true, message: '请输入验证码', trigger: 'blur' }, { pattern: /^\d{6}$/ }],
+        check: [{ validator (rule, value, callback) {
+          value ? callback() : callback(new Error('您需要了解用户协议及条款并勾选'))
+        } }]
+      }
+
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.myForm.validate(function (isOK) {
+        if (isOK) {
+          console.log('校验成功')
+        }
+      })
     }
   }
 }
