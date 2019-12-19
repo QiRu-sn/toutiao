d6d6f0d8b797e8f899f30774e651911d6f4c5a3c
QiRu <916331669@qq.com>
2019-12-19 22:38:10 +0800

    主页模块-头部结构及下拉菜单

---
+++ src/components/home/header.vue
@@ -0,0 +1,68 @@
+<template>
+<el-row class="headerContainer" type="flex" align="middle">
+    <!-- 头部左侧企业名称区域 -->
+    <el-col :span='12' class="left">
+        <i class="el-icon-s-unfold"></i>
+        <span>江苏传智播客教育科技股份有限公司</span>
+    </el-col>
+    <!-- 右侧区域 -->
+    <el-col :span='12' class="right">
+        <el-row type="flex" justify="end" align="middle" class="rightCon">
+            <!-- 右侧搜索框区域 -->
+            <el-tooltip content="搜索" placement="bottom">
+                <el-input style="width:188px; margin:0 20px;"
+                    placeholder="请输入搜索的文章内容"
+                    prefix-icon="el-icon-search">
+                </el-input>
+            </el-tooltip>
+            <!-- 右侧消息提示区域 -->
+            <el-tooltip style="margin-right:10px" content="消息" placement="bottom"><span>消息</span></el-tooltip>
+            <!-- 右侧头像区域 -->
+            <img src="../../assets/img/user.jpeg" alt="">
+            <!-- 右侧下拉菜单选项区域 -->
+            <el-dropdown>
+                    <span class="el-dropdown-link">这就是最好的<i class="el-icon-arrow-down el-icon--right"></i></span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>个人信息</el-dropdown-item>
+                        <el-dropdown-item>git地址</el-dropdown-item><hr/>
+                        <el-dropdown-item>退出</el-dropdown-item>
+                    </el-dropdown-menu>
+            </el-dropdown>
+        </el-row>
+    </el-col>
+</el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.headerContainer{
+    height: 50px;
+    background-color: #fff;
+    .left{
+        margin-left: 10px;
+        i{
+            font-size: 20px;
+            font-weight: 400;
+        }
+        span{
+            padding: 0 10px;
+        }
+    }
+    .right{
+        .rightCon{
+            img{
+                width: 35px;
+                height: 35px;
+                border-radius: 50%;
+                margin-right: 10px;
+                cursor: pointer;
+            }
+        }
+    }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,18 +3,22 @@
     <el-aside class="aside" style="width: 200px;">
       <asideCom></asideCom>
     </el-aside>
-    <div>
-       <el-header>Header</el-header>
-       <el-main>Main</el-main>
-    </div>
+      <el-container style="background-color:#f2f3f5">
+        <el-header style="padding:0">
+          <headerCom></headerCom>
+        </el-header>
+          <el-main>Main</el-main>
+      </el-container>
   </el-container>
 </template>
 
 <script>
-import asideCom from '../../components/home'
+import asideCom from '../../components/home/aside'
+import headerCom from '../../components/home/header'
 export default {
   components: {
-    asideCom
+    asideCom,
+    headerCom
   }
 }
 </script>
