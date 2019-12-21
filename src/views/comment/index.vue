43fdfb9061b56f2313ee9cb594c382bc986a07fd
QiRu <916331669@qq.com>
2019-12-21 21:54:44 +0800

    评论列表-评论数据加载到页面及状态过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
-      <el-card>
+      <el-card >
           <bread-crumb slot="header">
           <template slot="title">评论列表</template>
           </bread-crumb>
-          <el-table>
-              <el-table-column width="600" label="标题"></el-table-column>
-              <el-table-column label="评论状态"></el-table-column>
-              <el-table-column label="总评论数"></el-table-column>
-              <el-table-column label="粉丝评论数"></el-table-column>
-              <el-table-column label="操作"></el-table-column>
+          <el-table :data='list'>
+              <el-table-column width="600" label="标题" prop="title"></el-table-column>
+              <el-table-column :formatter="formatterStatus" label="评论状态" prop="comment_status"></el-table-column>
+              <el-table-column label="总评论数" prop="total_comment_count"></el-table-column>
+              <el-table-column label="粉丝评论数" prop="fans_comment_count"></el-table-column>
+              <el-table-column label="操作">
+                  <el-button type="text">修改</el-button>
+                  <el-button type="text">关闭</el-button>
+              </el-table-column>
           </el-table>
       </el-card>
   </div>
@@ -17,6 +20,29 @@
 
 <script>
 export default {
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    loadData () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(res => {
+        console.log(res)
+
+        this.list = res.data.results
+      })
+    },
+    formatterStatus (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.loadData()
+  }
 
 }
 </script>
