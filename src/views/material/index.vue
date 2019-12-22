291267e4fb8f54d2d930fa8e8372711ae325c52e
QiRu <916331669@qq.com>
2019-12-22 21:20:03 +0800

    素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,12 +1,30 @@
 <template>
   <el-card>
-      素材管理
+      <bread-crumb slot="header">
+        <template slot="title">素材管理</template>
+      </bread-crumb>
+      <el-row>
+        <el-radio-group  v-model="tabPosition">
+            <el-radio-button label="全部"></el-radio-button>
+            <el-radio-button label="收藏"></el-radio-button>
+        </el-radio-group>
+        <el-button style="float:right" type="primary">上传图片</el-button>
+      </el-row>
+      <el-row>
+          <el-card>
+              <!-- <img src="../../assets/img/login_bg1.jpeg" alt=""> -->
+          </el-card>
+      </el-row>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tabPosition: 'left'
+    }
+  }
 }
 </script>
 
